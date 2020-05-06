90ccf169fdee4fd32d58a72d67c6b595e8f67e55
Andrew Kloecker <andrew.kloecker@MacBook-Pro.local>
2020-05-06 08:23:22 -0400

    lab complete

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,21 @@
 # Add your code here
+class Dog
+  attr_reader :name
+  @@all = []
+  def initialize(name)
+    @name = name
+    save()
+  end
+  def self.all()
+    @@all
+  end
+  def self.clear_all()
+    @@all = []
+  end
+  def self.print_all()
+    @@all.each{ |var| puts var.name } 
+  end
+  def save()
+    @@all << self
+  end
+end
